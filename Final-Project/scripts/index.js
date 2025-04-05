4769016e70ef021cf6aa2dfcf1bf830d3049b672
TeAra <keitepai1@gmail.com>
2025-04-06 10:37:58 +1200

    final project week 5

---
+++ Final-Project/scripts/index.js
@@ -0,0 +1,25 @@
+const message = document.getElementById("message");
+const msToDays = 86400000;
+const dateToday = new Date();
+const lastVisitKey = "lastVisit";
+
+const displayVisitMessage = () => {
+    const lastVisit = localStorage.getItem(lastVisitKey);
+    if (!lastVisit) {
+        message.innerHTML = "Welcome to Health & Wellbeing! The website that offers a range of fitness and nutrition advice. We are pleased to have you here. We hope you enjoy exploring our website. Please feel free to subscribe at the bottom of our home page or get in contact with us via phone or email.";
+    } else {
+        const lastVisistDate = new Date(lastVisit);
+        const timeDifference = dateToday - lastVisistDate;
+        const daysSinceLastVisit = Math.floor(timeDifference / msToDays);
+
+        if (daysSinceLastVisit < 1) {
+            message.innerHTML = "Welcome back! We love to see people engage with our website and appreciate your support. Please feel free to get in touch if you have any queries regarding fitness.";
+        } else {
+            message.innerHTML = `Thank you so much for your continual support and engagement with our website! Your last visit was ${daysSinceLastVisit} day${daysSinceLastVisit > 1 ? "s" : ""
+                } ago. We appreciate you and hope you have a wonderful time exploring Fit For Life.`;
+        }
+    }
+    localStorage.setItem(lastVisitKey, dateToday.toISOString());
+};
+
+displayVisitMessage();
---
+++ practice/week 2/timer.js
@@ -0,0 +1,19 @@
+const countdownDisplay = document.getElementById('countdown');
+const startButton = document.getElementById('startButton');
+
+let timeLeft = 10;
+let countdownInterval;
+
+startButton.addEventListener('click', () => {
+    clearInterval(countdownInterval); // Clear any existing interval
+    timeLeft = 10; // Reset the timer
+    countdownInterval = setInterval(() => {
+        if (timeLeft >= 0) {
+            countdownDisplay.textContent = timeLeft;
+            timeLeft-=1;
+        } else {
+            clearInterval(countdownInterval); // Stop the countdown
+            countdownDisplay.textContent = "Time's up!"; // Display a message
+        }
+    }, 1000); 
+});
